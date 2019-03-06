7ece1ef4ee439005c27540ad18ccdcba93a63bd0
Christian Nguyen <cnguyen@marketo.com>
2019-03-06 00:09:46 -0800

    random

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import { connectRouter, routerMiddleware } from 'connected-react-router'
-import thunk from 'redux-thunk'
-import createHistory from 'history/createBrowserHistory'
+import { createStore, applyMiddleware, compose } from 'redux';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import thunk from 'redux-thunk';
+import createHistory from 'history/createBrowserHistory';
 import rootReducer from 'duck/rootReducer';
 
 export const history = createHistory()
@@ -11,7 +11,7 @@ const enhancers = []
 const middleware = [thunk, routerMiddleware(history)]
 
 if (process.env.NODE_ENV === 'development') {
-  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__
+  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
 
   if (typeof devToolsExtension === 'function') {
     enhancers.push(devToolsExtension())
@@ -20,11 +20,11 @@ if (process.env.NODE_ENV === 'development') {
 
 const composedEnhancers = compose(
   applyMiddleware(...middleware),
-  ...enhancers
+  ...enhancers,
 )
 
 export default createStore(
   connectRouter(history)(rootReducer),
   initialState,
-  composedEnhancers
+  composedEnhancers,
 )
